3a75f8934a161489fd44f2d58834d99dd7837472
Florian Liepe <florian.liepe@eraneos.com>
2026-08-06 22:31:10 +0200

    Fix Pages static asset base

--- runtime-config.js
+++ runtime-config.js
@@ -1,6 +1,6 @@
 window.INTELLECTUAL_TWIN_CONFIG = {
   apiBaseUrl: "",
-  assetBaseUrl: "/static",
+  assetBaseUrl: "",
   n8nChatWebhookUrl: "https://eraneos-agentic-platform.azurewebsites.net/webhook/meids/actor-twin/chat",
   n8nChatEnabled: true,
   staticPagesMode: true,
